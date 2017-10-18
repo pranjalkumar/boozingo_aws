a5e5399bd2febe42784288a697e17efb22d53445
pranjalkumar <pranjalkumar8228@gmail.com>
2017-10-18 13:40:05 +0530

    minor change 1

--- app/Http/Controllers/detailedInformationController.php
+++ app/Http/Controllers/detailedInformationController.php
@@ -16,7 +16,7 @@ class detailedInformationController extends Controller
                     'bar_address','bar_details','bar_cost','bar_geolocation','bar_time','bar_not_working_day','bar_contact','bar_booze_served','bar_food','bar_sitting_facility',
                     'bar_music','bar_payment','bar_ac')->get()->toArray();
                 $data_images=DB::table('bar_images')->where('bar_id','=',$type_id)->select('bar_images')->get()->toArray();
-                $result=json_encode(array('bars_detail'=>$data,'bar_images'=>$data_images));
+                $result=json_encode(array('bars'=>$data,'bar_images'=>$data_images));
                 if(count($data)==0)
                 {
                     return 'Sorry!! There is no result to display';
@@ -27,7 +27,7 @@ class detailedInformationController extends Controller
                 $data=DB::table('beer_shops')->where('id','=',$type_id)->select('beer_shop_name','beer_shop_address','beer_shop_details','beer_shop_geolocation','beer_shop_time',
                     'beer_shop_contact','beer_shop_not_working_day','beer_shop_booze_served','beer_shop_payment')->get()->toArray();
                 $data_images=DB::table('beer_shops_images')->where('beer_shop_id','=',$type_id)->select('beer_shop_images')->get()->toArray();
-                $result=json_encode(array('beer_shop_detail'=>$data,'beer_shop_images'=>$data_images));
+                $result=json_encode(array('beer_shop'=>$data,'beer_shop_images'=>$data_images));
                 if(count($data)==0)
                 {
                     return 'Sorry!! There is no result to display';
@@ -38,7 +38,7 @@ class detailedInformationController extends Controller
                 $data=DB::table('liquor_shops')->where('id','=',$type_id)->select('liquor_shop_name','liquor_shop_address','liquor_shop_details','liquor_shop_geolocation',
                     'liquor_shop_time','liquor_shop_contact','liquor_shop_not_working_day','liquor_shop_booze_served','liquor_shop_payment')->get()->toArray();
                 $data_images=DB::table('liquor_shop_images')->where('liquor_shop_id','=',$type_id)->select('liquor_shop_images')->get()->toArray();
-                $result=json_encode(array('liquor_shop_detail'=>$data_images,'liquor_shop_images'=>$data_images));
+                $result=json_encode(array('liquor_shop'=>$data_images,'liquor_shop_images'=>$data_images));
                 if(count($data)==0)
                 {
                     return 'Sorry!! There is no result to display';
@@ -49,7 +49,7 @@ class detailedInformationController extends Controller
                 $data=DB::table('lounges')->where('id','=',$type_id)->select('lounge_name','lounge_address','lounge_details','lounge_cost','lounge_geolocation','lounge_time','lounge_contact'
                 ,'lounge_not_working_day','lounge_booze_served','lounge_food','lounge_sitting_facility','lounge_music','lounge_payment','lounge_ac')->get()->toArray();
                 $data_images=DB::table('lounge_images')->where('lounge_id','=',$type_id)->select('lounge_images')->get()->toArray();
-                $result=json_encode(array('lounge_detail'=>$data,'lounge_images'=>$data_images));
+                $result=json_encode(array('lounge'=>$data,'lounge_images'=>$data_images));
                 if(count($data)==0)
                 {
                     return 'Sorry!! There is no result to display';
@@ -60,7 +60,7 @@ class detailedInformationController extends Controller
                 $data=DB::table('model_shops')->where('id','=',$type_id)->select('model_shop_name','model_shop_address','model_shop_details','model_shop_geolocation',
                     'model_shop_time','model_shop_contact','model_shop_not_working_day','model_shop_booze_served','model_shop_payment')->get()->toArray();
                 $data_images=DB::table('model_shop_images')->where('model_shop_id','=',$type_id)->select('model_shop_images')->get()->toArray();
-                $result=json_encode(array('model_shop_detail'=>$data,'model_shop_images'=>$data_images));
+                $result=json_encode(array('model_shop'=>$data,'model_shop_images'=>$data_images));
                 if(count($data)==0)
                 {
                     return 'Sorry!! There is no result to display';
